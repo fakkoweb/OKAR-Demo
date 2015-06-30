005fc7d073efd9f885920e131be84068c5afc120
fakkoweb <fakkoweb@libero.it>
2015-06-30 17:59:29 +0200

    Update on cmake scripts

--- cmake_install_2.8.12.sh
+++
@@ -1,6 +0,0 @@
-export fn=/tmp/cmake.sh && ls $fn
-(echo "use previous $fn? Enter for yes, ctrl+d for no." && read) || (wget -O $fn http://www.cmake.org/files/v2.8/cmake-2.8.12-Linux-i386.sh 1>&2)
-cd /tmp
-sudo bash ${fn}
-#echo sudo ln -f -s /tmp/cmake*/bin/cmake /usr/local/bin/cmake
-cd -
---
+++ install_here_cmake2.8.12_linux.sh
@@ -0,0 +1,7 @@
+export fn=./cmake_install.sh
+wget -O $fn http://www.cmake.org/files/v2.8/cmake-2.8.12-Linux-i386.sh 1>&2
+echo "Press Q and then say yes to all. Press Enter to continue." && read
+bash ${fn}
+ln -f -s ./cmake-*/bin/cmake mycmake
+rm -R ./cmake_install.sh
+echo "Cmake installed in current directory. A link has been created as well to use './mycmake' command right away."
