3763cb3316240964ee78f9ab8159b68abd7ab976
Dario Facchini <fakkoweb@libero.it>
2015-07-01 15:23:28 +0200

    fix local cmake installer script to add a link to ccmake also

--- install_here_cmake2.8.12_linux.sh
+++ install_here_cmake2.8.12_linux.sh
@@ -3,5 +3,6 @@ wget -O $fn http://www.cmake.org/files/v2.8/cmake-2.8.12-Linux-i386.sh 1>&2
 echo "Press Q and then say yes to all. Press Enter to continue." && read
 bash ${fn}
 ln -f -s ./cmake-*/bin/cmake mycmake
+ln -f -s ./cmake-*/bin/ccmake myccmake
 rm -R ./cmake_install.sh
 echo "Cmake installed in current directory. A link has been created as well to use './mycmake' command right away."
